2560870218f5c642419b89e5de4ca0210fe031c9
Андрей <asumilov38@gmail.com>
2026-04-28 19:34:15 +0300

    app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-"use strict";
 const SUPABASE_URL = 'https://ymcfgtxfuxhydzjeibll.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_7nC0W-548Id_SCMs2dyKDA_SqAShFiJ';
 const CREATOR_PHONE = '+79049584282';
+
+"use strict";
 // ==========================================
 // GREENFREELANCE v5 — ПОЛНЫЙ ФУНКЦИОНАЛ
 // ==========================================
